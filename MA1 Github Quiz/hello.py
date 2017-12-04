0504a1b05234a00c489c4cb7280b382761acb4fb
kevinchyun3502 <32549493+kevinchyun3502@users.noreply.github.com>
2017-12-04 08:24:16 -0500

    Interactive Version

---
+++ MA1 Github Quiz/hello.py
@@ -0,0 +1,5 @@
+import sys
+# What does end='' do in the print statements? 
+# Respond here: end='' allows you to print more after you printed already. Also, this allows all the printed parts to be printed in the same line, not separate lines. 
+name = input('What is your name')
+print ('Hello,' + name)
